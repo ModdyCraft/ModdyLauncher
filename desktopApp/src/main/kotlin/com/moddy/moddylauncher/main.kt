f3f31340f8321b3fdd0df4ab2cd44d988ac812de
ModdyDev <romeroeinsten@gmail.com>
2026-07-19 02:06:06 -0500

    Primer Commit

---
+++ desktopApp/src/main/kotlin/com/moddy/moddylauncher/main.kt
@@ -0,0 +1,13 @@
+package com.moddy.moddylauncher
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ModdyLauncher",
+    ) {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/moddy/moddylauncher/LauncherPaths.kt
@@ -0,0 +1,5 @@
+package com.moddy.moddylauncher
+
+object LauncherPaths {
+
+}

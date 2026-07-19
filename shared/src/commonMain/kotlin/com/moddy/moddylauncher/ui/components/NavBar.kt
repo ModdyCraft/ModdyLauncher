8825e5815368b00d8981b1db55cfa01258964241
ModdyDev <romeroeinsten@gmail.com>
2026-07-19 03:11:55 -0500

    NAVBAR en inicios

--- shared/src/commonMain/kotlin/com/moddy/moddylauncher/ui/components/NavBar.kt
+++ shared/src/commonMain/kotlin/com/moddy/moddylauncher/ui/components/NavBar.kt
@@ -1,30 +1,40 @@
 package com.moddy.moddylauncher.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.fastForEachIndexed
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun NavBar() {
     val navList = listOf("Instances", "Console", "Auth", "Settings")
 
     Row(
-        modifier = Modifier.fillMaxWidth().height(12.dp),
+        modifier = Modifier.fillMaxWidth().height(20.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         navList.fastForEachIndexed { i, nav ->
-            Text(nav)
-            if (i >= navList.lastIndex) {
-                VerticalDivider()
+            Text(
+                nav,
+                style = TextStyle(fontSize = 12.sp, textAlign = TextAlign.Center),
+                modifier = Modifier
+                    .clickable {}
+                    .weight(1f)
+                    .fillMaxHeight()
+            )
+            if (i <= navList.lastIndex) {
+                VerticalDivider(modifier = Modifier.fillMaxHeight())
             }
         }
     }

da276af82d38203dd80d0cf88451d0cb27a663a8
ModdyDev <romeroeinsten@gmail.com>
2026-07-23 10:37:14 -0500

    First intencional fix

--- shared/src/commonMain/kotlin/com/moddy/moddylauncher/App.kt
+++ shared/src/commonMain/kotlin/com/moddy/moddylauncher/App.kt
@@ -3,12 +3,12 @@ package com.moddy.moddylauncher
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.moddy.moddylauncher.ui.components.NavBar
+import com.moddy.moddylauncher.ui.home.HomeScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        NavBar()
+        HomeScreen()
     }
 }
---
+++ shared/src/commonMain/kotlin/com/moddy/moddylauncher/ui/home/HomeScreen.kt
@@ -0,0 +1,32 @@
+package com.moddy.moddylauncher.ui.home
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Nickname") },
+            placeholder = { Text("LOLES") },
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedButton(
+            onClick = {},
+        ) {
+            Text("PLAY")
+        }
+    }
+}

2f61e0a5d13240d70ac40f3427181431fcb77873
ModdyDev <romeroeinsten@gmail.com>
2026-07-28 14:50:01 -0500

    Probando SplashScreen

--- shared/src/commonMain/kotlin/com/moddy/moddylauncher/ui/splash/SplashScreen.kt
+++ shared/src/commonMain/kotlin/com/moddy/moddylauncher/ui/splash/SplashScreen.kt
@@ -3,12 +3,20 @@ package com.moddy.moddylauncher.ui.splash
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(
+    viewModel: SplashScreenViewModel = koinViewModel()
+) {
+
+    val text by viewModel.consola.collectAsState(initial = "")
+
     Text(
-        "",
+        text = text,
         modifier = Modifier.fillMaxSize(),
     )
 }
